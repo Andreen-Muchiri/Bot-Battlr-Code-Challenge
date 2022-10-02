2e7ad98d0e59e7361c94ce83b79b2d740590cd97
Andreen-Muchiri <andreenshii@gmail.com>
2022-10-02 03:12:03 +0300

    Complete Assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, {useEffect , useState} from "react";
 import BotsPage from "./BotsPage";
 
 function App() {
+ //useState...
+  const [botData , setBotData] = useState([]);
+  const[viewBotData , setViewBotData]= useState([false]);
+
+  //useEffect...
+  //fetch data using GET method 
+  useEffect(() => {
+    fetch("http://localhost:8002/bots")
+        .then((resp) => resp.json())
+        .then((bots) => {
+            console.log(bots);
+            setBotData(bots);
+        })
+       .catch((error) => {
+            console.log(error.message);
+        });
+}, [viewBotData]);
+
+
   return (
     <div className="App">
-      <BotsPage />
+      <BotsPage 
+      botData = {botData}
+      setBotData = {setBotData}
+      setViewBotData={setViewBotData} 
+      />
     </div>
   );
 }
 
 export default App;
+
+
